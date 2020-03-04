d5b611d4afd1307530f8da59c529b92b3cb94ad7
silaxan <ssilaxan135@gmail.com>
2020-03-04 14:36:50 +0530

    multification table

---
+++ 04.03.2020/MultificationTable.java
@@ -0,0 +1,24 @@
+public class MultificationTable{
+	public static void main(String args[]){
+		int num = 5;
+			for(int i = 1;i<=12;i++){
+				int mul= i*num;
+				System.out.println(i + "x" + num + "=" + mul);
+			}
+			
+	}
+}
+
+//output
+//1x5=5
+//2x5=10
+//3x5=15
+//4x5=20
+//5x5=25
+//6x5=30
+//7x5=35
+//8x5=40
+//9x5=45
+//10x5=50
+//11x5=55
+//12x5=60
